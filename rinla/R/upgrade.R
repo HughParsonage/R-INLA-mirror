bef1fa44141867459244fe32738287875fa2882c
Havard Rue <havard.rue@math.ntnu.no>
2016-11-14 18:45:23 +0100

    Fixed model=rgeneric to work with rep and group

---
+++ rinla/R/upgrade.R
@@ -0,0 +1,57 @@
+## Export: inla.upgrade inla.update
+
+##!\name{inla.upgrade}
+##!\alias{inla.upgrade}
+##!\alias{inla.update}
+##!
+##!\title{Upgrade the INLA-package}
+##!
+##!\description{Functions to upgrade the \code{INLA}-package to the
+##!  current version.}
+##!
+##!\usage{
+##!inla.upgrade(lib = NULL, testing=FALSE, ask = TRUE)
+##!inla.update(lib = NULL, testing=FALSE, ask = TRUE)
+##!}
+##!
+##!\arguments{
+##!  \item{lib}{Location to install the library.}
+##!  \item{testing}{If \code{TRUE}, then look for a test-version if the
+##!    INLA-package.}
+##!  \item{ask}{same argument as in \code{update.packages}}
+##!}
+##!\value{\code{inla.upgrade} will update the INLA package to the current version, 
+##! and \code{inla.update} do the same 
+##!  for backward compatibility. This function is simple wrapper for
+##!  \code{update.packages} using the INLA repository.}
+##!\author{Havard Rue \email{hrue@math.ntnu.no}}
+##!\seealso{\code{update.packages}}
+
+### The upgrade utility
+
+`inla.update` = function(lib = NULL, testing = FALSE, ask = TRUE)
+{
+    inla.upgrade(lib=lib, testing=testing, ask = ask)
+}
+
+`inla.upgrade` = function(lib = NULL, testing = FALSE, ask = TRUE)
+{
+    repo=c(CRAN = "https://cran.rstudio.com",
+        INLA = paste("https://www.math.ntnu.no/inla/R/",
+            (if (testing) "testing" else "stable"),  sep=""))
+    if (require("INLA", quietly = TRUE, lib.loc = lib, character.only=TRUE)) {
+        suppressWarnings(new.pack <- any(old.packages(repos = repo)[,1] == "INLA"))
+        if (new.pack) {
+            suppressWarnings(update.packages(repos = repo, oldPkgs = "INLA", ask = ask))
+            cat("\n *** If the INLA-package was updated, you need to restart R and load it again.\n\n")
+        } else {
+            cat("\n *** You already have the latest version.\n\n")
+        }
+    } else {
+        install.packages(pkgs = "INLA", lib = lib, repos = repo,
+                         dependencies = TRUE)
+        library("INLA")
+    }
+
+    return (invisible())
+}
